dad70d4fbf502270359dca7c8ee07bcf2f3b5c56
Alexander Surma <surma@surmair.de>
2014-03-24 21:17:28 +0100

    Add 'win rate over time' and 'champions played over time'

--- web/src/js/config.js
+++ web/src/js/config.js
@@ -1,5 +1,5 @@
 angular.module('lolkaiser').constant('CONFIG', {
-	"servers": [
+	servers: [
 		{
 			id: 'euw',
 			name: 'Europe West'
@@ -9,88 +9,88 @@ angular.module('lolkaiser').constant('CONFIG', {
 			name: 'North America'
 		}
 	],
-	"gametypes": [
+	gametypes: [
 		{
-			name: "Normal 5v5",
+			name: 'Normal 5v5',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.game_type == "Normal 5v5";	
+						return e.game_type == 'Normal 5v5';
 					})
 					.__wrapped__;
 			}
 		},
 		{
-			name: "Ranked Solo 5v5",
+			name: 'Ranked Solo 5v5',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.game_type == "Ranked Solo 5v5";	
+						return e.game_type == 'Ranked Solo 5v5';
 					})
 					.__wrapped__;
 			}
 		},
 		{
-			name: "Co-Op Vs AI",
+			name: 'Co-Op Vs AI',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.game_type == "Co-Op Vs AI";	
+						return e.game_type == 'Co-Op Vs AI';
 					})
 					.__wrapped__;
 			}
 		}
 	],
-	"timepoints": [
+	timepoints: [
 		{
-			name: "After start of Season 3",
+			name: 'After start of Season 3',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.timestamp > "2013-02-01T00:00:00Z";
+						return e.timestamp > '2013-02-01T00:00:00Z';
 					})
 					.__wrapped__;
 			}
 		},
 		{
-			name: "Before start of Season 4",
+			name: 'Before start of Season 4',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.timestamp < "2013-01-17T00:00:00Z";
+						return e.timestamp < '2013-01-17T00:00:00Z';
 					})
 					.__wrapped__;
 			}
 		},
 		{
-			name: "After start of Season 4",
+			name: 'After start of Season 4',
 			f: function(data) {
 				return _(data)
 					.filter(function(e) {
-						return e.timestamp > "2013-01-17T00:00:00Z";
+						return e.timestamp > '2013-01-17T00:00:00Z';
 					})
 					.__wrapped__;
 			}
 		},
 		{
-			name: "Last 50 games",
+			name: 'Last 50 games',
 			f: function(data) {
 				return _(data)
-					.sortBy("timestamp")
+					.sortBy('timestamp')
 					.last(50)
 					.__wrapped__;
 			}
 		}
 	],
-	"mappings": [
+	mappings: [
 		{
-			name: "Win rate per Champion",
+			name: 'Win rate per Champion',
 			f: function(data) {
 				return {
 					type: 'bar',
 					data: [
 						{
-							key: "Win rate per Champion",
+							key: 'Win rate per Champion',
 							values: _(data)
 								.reduce(function(acc, e){
 									var idx = _(acc).findIndex({'champion': e.champion});
@@ -114,6 +114,68 @@ angular.module('lolkaiser').constant('CONFIG', {
 								})
 						}
 					]
+				};
+			},
+		},
+		{
+			name: 'Win rate over time',
+			f: function(data) {
+				var sliceWidth = 3;
+				return {
+					type: 'line',
+					data: [
+						{
+							key: 'Win rate over time',
+							values: _(data)
+								.sortBy('timestamp')
+								.reverse()
+								.map(function(e, i, c) {
+									return c.slice(i, i+sliceWidth);
+								})
+								.filter(function(e) {
+									return _(e).size() == sliceWidth;
+								})
+								.map(function(e) {
+									return _(e).filter('win').size()/sliceWidth;
+								})
+								.map(function(e, i) {
+									return [i, Math.floor(e*10000)/100];
+								})
+								.__wrapped__
+						}
+					]
+				}
+			}
+		},
+		{
+			name: 'Champions played over time',
+			f: function(data) {
+				var sliceWidth = 3;
+				var champions = _(data).map('champion').uniq().__wrapped__;
+				data = _(data)
+					.sortBy('timestamp')
+					.reverse()
+					.map(function(e, i, c) {
+						return c.slice(i, i+sliceWidth);
+					})
+					.map(function(e) {
+						return _(e).countBy('champion').__wrapped__;
+					})
+					.__wrapped__
+
+				return {
+					type: 'stacked-area',
+					style: 'expand',
+					c: champions,
+					data: _(champions).map(function(c) {
+						return {
+							key: c,
+							values: _(data).map(function(e, i) {
+								return [i, e[c] || 0];
+							}).__wrapped__
+						}
+					})
+					.__wrapped__
 				};
 			},
 		}
